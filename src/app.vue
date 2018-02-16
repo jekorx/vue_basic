40540875f47954cf8eecc05673825db86fcaa1f0
jekorx <wdg404@163.com>
2018-02-16 20:56:49 +0800

    replace root element

--- src/app.vue
+++ src/app.vue
@@ -19,22 +19,20 @@ export default {
   }
 }
 </script>
-<style lang="stylus" scoped>
-#app {
+<style lang="stylus">
+#app
   position absolute
   left 0
   right 0
   top 0
   bottom 0
-}
-#cover {
-  position absolute
-  left 0
-  right 0
-  top 0
-  bottom 0
-  background-color #999
-  opacity .9
-  z-index -1
-}
+  #cover
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
 </style>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import App from './app.vue'
 
 import './assets/styles/global.styl'
 
-const root = document.createElement('div')
-
-const body = document.body
-if (body.children.length > 0) {
-  body.insertBefore(root, body.children[0])
-} else {
-  body.appendChild(root)
-}
+const root = document.getElementById('root')
 
 new Vue({
   render: h => h(App)
