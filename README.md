8a92ac58890efea22e1cd7d7204915019dee4133
jekorx <wdg404@163.com>
2018-02-27 11:11:04 +0800

    修复autoprefixer无法生效的bug，增加生产模式抽取.vue中样式

--- README.md
+++
@@ -1,7 +0,0 @@
-# vuetodo
-
-vue2 + webpack，基础使用
-
-包含jsx
-
-效果展示https://jekorx.github.io/vue_basic/
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './app.vue'
-
-import './assets/styles/global.styl'
-
-new Vue({
-  render: h => h(App)
-}).$mount('#root')
